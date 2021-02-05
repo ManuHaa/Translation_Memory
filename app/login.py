cf906588275ab5bba842c25a459f342ea710deac
Manuel Haugg <manuel.haugg@hs-furtwangen.de>
2021-02-05 12:47:32 +0100

    .

--- app/login.py
+++ app/login.py
@@ -4,22 +4,37 @@ import sys
 auth = Atuhtenticator()
 
 class LoginMask:
-    
+
     def loginMaskOperator(self):
+        operator = { "operator" : None}
         state = input("Wollen Sie sich anmelden oder als Benutzer fortfahren? (y/n)")
         if state == "y":
             un = input("Benutzername eingeben:")
             pw = input("Passwort eingeben:")
             if auth.isTranslator(un, pw):
                 print("Wilkommen Translator!")
+                operator['operator'] = "translator"
+                return operator
             elif auth.isAdmin(un, pw):
                 print("Wilkommen Admin!")
+                operator['operator'] = "admin"
+                return operator
             else: 
                 continueState = input("Ups, Sie scheinen nicht registriert zu sein. Wollen Sie als User fortfahren? (y/n)")
                 if continueState == "y":
                     print("Wilkommen User!")
+                    operator['operator'] = "user"
+                    return operator
                 else:
                     print("Auf Wiedersehen!")
                     sys.exit()
-        else:
+        elif state == "n":
             print("Hallo User!")
+            operator['operator'] = "user"
+            return operator
+        else:
+            print("Ups, Ihre Eingabe war fehlerhaft.")
+            sys.exit()
+
+
+            
