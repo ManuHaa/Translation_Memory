1f47ea1386ea0ba089ee1fe372b33379b729a432
Manuel Haugg <manuel.haugg@hs-furtwangen.de>
2021-02-09 22:04:40 +0100

    .

--- app/login/login.py
+++ app/login/login.py
@@ -1,42 +1,42 @@
 from login.authentication import Atuhtenticator
 from operators.userOperator import UserOperator
+from utils import Colors as color
 import sys
 
 auth = Atuhtenticator()
 user = UserOperator()
-color = '\033[93m'
 
 class LoginMask:
 
     def loginMaskOperator(self):
         operator = { "operator" : None}
-        state = input("TranslationMemory\nWilkommen! \nWollen Sie sich anmelden oder als Benutzer fortfahren? (y/n) \nEingabe: ")
+        state = input(color.blue + "TranslationMemory\n" + color.end + "Wilkommen!\nWollen Sie sich anmelden oder als Benutzer fortfahren? (y/n) \n" + color.yellow + "Eingabe: " +  color.end)
         if state == 'y':
             username = input("Benutzername eingeben: ")
             password = input("Passwort eingeben: ")
             if auth.isTranslator(username, password):
-                print("Wilkommen Translator!")
+                print(color.green + "Wilkommen Translator!" + color.end)
                 operator['operator'] = "translator"
                 return operator
             elif auth.isAdmin(username, password):
-                print("Wilkommen Admin!")
+                print(color.green + "Wilkommen Admin!" + color.end)
                 operator['operator'] = "admin"
                 return operator
             else: 
-                continueState = input("Ups, Sie scheinen nicht registriert zu sein. Wollen Sie als User fortfahren?(y/n) \nEingabe: ")
+                continueState = input(color.yellow + "Ups, Sie scheinen nicht registriert zu sein. Wollen Sie als User fortfahren?(y/n) \n" + color.end + "Eingabe: ")
                 if continueState == 'y':
-                    print("Wilkommen User!")
+                    print(color.green + "Wilkommen User!" + color.end)
                     operator['operator'] = "user"
                     return operator
                 else:
-                    print("Auf Wiedersehen!")
+                    print(color.red + "Auf Wiedersehen!" + color.end)
                     sys.exit()
         elif state == 'n':
-            print("Wilkommen User!")
+            print(color.green + "Wilkommen User!" + color.end)
             operator['operator'] = "user"
             return operator
         else:
-            print("Ups, Ihre Eingabe war fehlerhaft.")
+            print(color.red + "Ups, Ihre Eingabe war fehlerhaft." + color.end)
             sys.exit()
 
 
--- app/utils.py
+++ app/utils.py
@@ -8,6 +8,15 @@ import array
 uuids = []
 basePath = os.path.dirname(os.path.abspath(__file__))
 
+#-color codes
+class Colors:
+    blue = '\033[94m'
+    green = '\033[92m'
+    yellow = '\033[93m'
+    red = '\033[91m'
+    end = '\033[0m'
+    bold = '\033[1m'
+    underline = '\033[4m'
 
 #-returns already generated 
 def get_used_uuids() -> list:
